1625c685fea537f1282fca94d18e8b7c91d70fb0
tn-17 <59483688+tn-17@users.noreply.github.com>
2026-09-11 22:53:49 -0400

    fix: remove flash nvim

--- .config/astrovim/lua/community.lua
+++ .config/astrovim/lua/community.lua
@@ -8,7 +8,6 @@ return {
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.file-explorer.yazi-nvim" },
   { import = "astrocommunity.search.grug-far-nvim" },
